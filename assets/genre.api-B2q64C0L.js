eac69c08515131706f9c745aa5938217b2dc6877
MeinLiX <MeinLiX@users.noreply.github.com>
2026-07-05 09:54:55 +0000

    deploy: 2f66afb2dbd3d287378c10b94cbeaa96186e10d0 HalerkaDigital/DaliBudeDub@2f66afb2dbd3d287378c10b94cbeaa96186e10d0

---
+++ assets/genre.api-B2q64C0L.js
@@ -0,0 +1,2 @@
+import{x as e}from"./index-CwmJr4Xs.js";function r(){return e.get("/genres")}export{r as f};
+//# sourceMappingURL=genre.api-B2q64C0L.js.map
--- assets/genre.api-DsmEZ-FU.js
+++
@@ -1,2 +0,0 @@
-import{x as e}from"./index-D9m8atTt.js";function r(){return e.get("/genres")}export{r as f};
-//# sourceMappingURL=genre.api-DsmEZ-FU.js.map
