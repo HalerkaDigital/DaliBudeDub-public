4d4d9e267a592d47cda4df695e2674020fd81eea
MeinLiX <MeinLiX@users.noreply.github.com>
2026-07-05 20:38:02 +0000

    deploy: 92bb6471f85f1d6477bf3b41a9b3e2cd03e82e9d HalerkaDigital/DaliBudeDub@92bb6471f85f1d6477bf3b41a9b3e2cd03e82e9d

--- assets/genre.api-CYYgvFmq.js
+++
@@ -1,2 +0,0 @@
-import{x as e}from"./index-ChB-ClBV.js";function r(){return e.get("/genres")}export{r as f};
-//# sourceMappingURL=genre.api-CYYgvFmq.js.map
---
+++ assets/genre.api-DXua-vMV.js
@@ -0,0 +1,2 @@
+import{x as e}from"./index-Ba07CNiH.js";function r(){return e.get("/genres")}export{r as f};
+//# sourceMappingURL=genre.api-DXua-vMV.js.map
