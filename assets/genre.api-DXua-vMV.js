cdbdfcfed0644c8084e331b5d742e0e238695015
MeinLiX <MeinLiX@users.noreply.github.com>
2026-07-05 21:03:04 +0000

    deploy: ef34439b91020cdcfab95314c0baee849727c47a HalerkaDigital/DaliBudeDub@ef34439b91020cdcfab95314c0baee849727c47a

--- assets/genre.api-DXua-vMV.js
+++
@@ -1,2 +0,0 @@
-import{x as e}from"./index-Ba07CNiH.js";function r(){return e.get("/genres")}export{r as f};
-//# sourceMappingURL=genre.api-DXua-vMV.js.map
---
+++ assets/genre.api-QiDKQ4pQ.js
@@ -0,0 +1,2 @@
+import{x as e}from"./index-Jz99C1Ki.js";function r(){return e.get("/genres")}export{r as f};
+//# sourceMappingURL=genre.api-QiDKQ4pQ.js.map
