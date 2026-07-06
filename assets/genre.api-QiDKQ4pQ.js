5fa250474744be37d8bee6cd1a607ae3e4cfa837
MeinLiX <MeinLiX@users.noreply.github.com>
2026-07-06 08:06:57 +0000

    deploy: 8383f2178cf9949bf5821ff21adfb910156268db HalerkaDigital/DaliBudeDub@8383f2178cf9949bf5821ff21adfb910156268db

--- assets/genre.api-QiDKQ4pQ.js
+++
@@ -1,2 +0,0 @@
-import{x as e}from"./index-Jz99C1Ki.js";function r(){return e.get("/genres")}export{r as f};
-//# sourceMappingURL=genre.api-QiDKQ4pQ.js.map
---
+++ assets/title.api-Aqd8mrG4.js
@@ -0,0 +1,2 @@
+import{x as e}from"./index-C5ZHeGH9.js";function n(t){return e.get(`/titles/${t}`)}function a(t,r){return e.put(`/titles/${t}/rating`,{score:r})}export{n as f,a as r};
+//# sourceMappingURL=title.api-Aqd8mrG4.js.map
