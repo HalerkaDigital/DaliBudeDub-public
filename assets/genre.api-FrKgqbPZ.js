8d9ba30e0239bd0dfe19103afdc80192022019a5
MeinLiX <MeinLiX@users.noreply.github.com>
2026-07-09 09:49:13 +0000

    deploy: ccd11f0e386f09f15a985d9f18c0d798c682537a HalerkaDigital/DaliBudeDub@ccd11f0e386f09f15a985d9f18c0d798c682537a

---
+++ assets/genre.api-FrKgqbPZ.js
@@ -0,0 +1,1 @@
+import{x as e}from"./index-DVI5O7cK.js";function r(){return e.get("/genres")}export{r as f};
--- assets/genre.api-X4iJ3YmB.js
+++
@@ -1,1 +0,0 @@
-import{x as e}from"./index-B8lqqg7V.js";function r(){return e.get("/genres")}export{r as f};
