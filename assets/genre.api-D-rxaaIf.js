57de1ca2cd42ecd61688aef2ffa2564847c45a0b
MeinLiX <MeinLiX@users.noreply.github.com>
2026-07-12 15:06:55 +0000

    deploy: 9e6968479b38e830e0dab089368818719987bb52 HalerkaDigital/DaliBudeDub@9e6968479b38e830e0dab089368818719987bb52

---
+++ assets/genre.api-D-rxaaIf.js
@@ -0,0 +1,1 @@
+import{x as e}from"./index-omUP_kFy.js";function r(){return e.get("/genres")}export{r as f};
--- assets/genre.api-FrKgqbPZ.js
+++
@@ -1,1 +0,0 @@
-import{x as e}from"./index-DVI5O7cK.js";function r(){return e.get("/genres")}export{r as f};
