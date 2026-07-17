c816e4d20b17f1165cb6368ab748182287447e24
MeinLiX <MeinLiX@users.noreply.github.com>
2026-07-17 16:53:56 +0000

    deploy: e8f0518f65ac8b3519e32343329b9ff398f56819 HalerkaDigital/DaliBudeDub@e8f0518f65ac8b3519e32343329b9ff398f56819

--- assets/genre.api-DJWyEyjg.js
+++
@@ -1,1 +0,0 @@
-import{x as e}from"./index-JnI1Xswa.js";function r(){return e.get("/genres")}export{r as f};
---
+++ assets/genre.api-N1efFxHw.js
@@ -0,0 +1,1 @@
+import{x as e}from"./index-C-E-WUcv.js";function r(){return e.get("/genres")}export{r as f};
