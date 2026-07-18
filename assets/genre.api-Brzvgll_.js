b649f31c215fe2d317eca32dff28c61c1926b00d
MeinLiX <MeinLiX@users.noreply.github.com>
2026-07-18 17:33:06 +0000

    deploy: f31f4b0c618af819608193c259a378f5d6af1c50 HalerkaDigital/DaliBudeDub@f31f4b0c618af819608193c259a378f5d6af1c50

---
+++ assets/genre.api-Brzvgll_.js
@@ -0,0 +1,1 @@
+import{x as e}from"./index-CxYiL9KM.js";function r(){return e.get("/genres")}export{r as f};
--- assets/genre.api-N1efFxHw.js
+++
@@ -1,1 +0,0 @@
-import{x as e}from"./index-C-E-WUcv.js";function r(){return e.get("/genres")}export{r as f};
