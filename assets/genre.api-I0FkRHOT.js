51bd413781b63c5f4d3978c98f1d250bca768782
MeinLiX <MeinLiX@users.noreply.github.com>
2026-07-23 08:11:46 +0000

    deploy: 9332391a45656da71f989cdc1d20b70e285eac1b HalerkaDigital/DaliBudeDub@9332391a45656da71f989cdc1d20b70e285eac1b

--- assets/genre.api-I0FkRHOT.js
+++
@@ -1,1 +0,0 @@
-import{z as e}from"./index-BIhVpQGS.js";function r(){return e.get("/genres")}export{r as f};
--- assets/title.api-W5v8lINF.js
+++
@@ -1,1 +0,0 @@
-import{z as e}from"./index-BIhVpQGS.js";function n(t){return e.get(`/titles/${t}`)}function a(t,r){return e.put(`/titles/${t}/rating`,{score:r})}export{n as f,a as r};
